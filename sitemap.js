e51f080ae95bddd4748669ba6df035cad3dd1357
naveen200848 <164338751+naveen200848@users.noreply.github.com>
2025-12-22 21:26:20 +0530

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -2,22 +2,19 @@ import { getFirestore, collection, getDocs, query, orderBy } from "https://www.g
 
 export async function generateSitemap() {
     const db = getFirestore();
-    const querySnapshot = await getDocs(query(collection(db, "news"), orderBy("date", "desc")));
+    const snap = await getDocs(query(collection(db, "news"), orderBy("date", "desc")));
 
     let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
     
-    // Core Static Pages
-    ["index.html", "ainews.html", "whatsapp-automation.html","whatsapp-recovery.html"].forEach(page => {
-        xml += `  <url>\n    <loc>https://www.idigitalworks.com/${page}</loc>\n    <priority>1.0</priority>\n  </url>\n`;
+    ["index.html", "ainews.html", "whatsapp-automation.html", "whatsapp-recovery.html"].forEach(p => {
+        xml += `  <url>\n    <loc>https://www.idigitalworks.com/${p}</loc>\n    <priority>1.0</priority>\n  </url>\n`;
     });
 
-    // Dynamic AI News
-    querySnapshot.forEach((doc) => {
-        const post = doc.data();
-        const date = post.date ? new Date(post.date.seconds * 1000).toISOString().split('T')[0] : new Date().toISOString().split('T')[0];
-        xml += `  <url>\n    <loc>https://www.idigitalworks.com/ainews.html?id=${doc.id}</loc>\n    <lastmod>${date}</lastmod>\n    <priority>0.8</priority>\n  </url>\n`;
+    snap.forEach((doc) => {
+        const p = doc.data();
+        const d = p.date ? new Date(p.date.seconds * 1000).toISOString().split('T')[0] : new Date().toISOString().split('T')[0];
+        xml += `  <url>\n    <loc>https://www.idigitalworks.com/ainews.html?id=${doc.id}</loc>\n    <lastmod>${d}</lastmod>\n    <priority>0.8</priority>\n  </url>\n`;
     });
 
-    xml += `</urlset>`;
-    return xml;
+    return xml + `</urlset>`;
 }
